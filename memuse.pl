f01b99f48b6ed4f948121e0e37f93e58bce43be4
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2021-06-27 11:19:25 -0400

    Work on memory usage tester

--- memuse.pl
+++ memuse.pl
@@ -6,32 +6,50 @@ use Memory::Usage;
 my $mu = Memory::Usage->new();
 use Marpa::R2;
 
-my $tokens = 1;
+my $symbols = 1;
 my $input_size = 20;
 
 $input_size = $ARGV[0] if defined $ARGV[0];
-$tokens = $ARGV[1] if defined $ARGV[1];
-say STDERR "$tokens $input_size";
+$symbols = $ARGV[1] if defined $ARGV[1];
+say STDERR "s=$symbols t=$input_size";
 
 # Record amount of memory used by current process
 $mu->record('BEFORE');
 
-my $source = <<'END_OF_DSL';
+my @source = ();
+push @source,  <<'END_OF_DSL';
 :default ::= action => [name,values]
 lexeme default = action => [ start, length, value ]
     latm => 1
 
 # standard part of grammar
-top ::= item*
+top ::= guarded_items
+top ::= guarded_unicorns
+guarded_items ::= item_guard items
+item_guard ~ 'x'
+guarded_unicorns ::= unicorn_guard unicorns
+unicorn_guard ~ [^\d\D]
+unicorn_token ~ [^\d\D]
+items ::= item*
+unicorns ::= unicorn*
 item ::= token token ~ 'x'
-item ::= unicorn1 unicorn1 ~ [\D\d]
 END_OF_DSL
+
+for (my $i = 0; $i < $symbols; $i++) {
+  push @source, "unicorn ::= unicorn$i unicorn$i ::= unicorn_token";
+}
+my $source = join "\n", @source;
 
 my $grammar = Marpa::R2::Scanless::G->new( { source => \$source } );
 my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
 
+# Record amount of memory used by current process
+$mu->record('DURING');
+
 my $input = 'x' x $input_size;
 $recce->read(\$input);
+# say $recce->show_progress(0,1);
+# say $recce->show_progress(-3, -1);
 
 # Record amount in use afterwards
 $mu->record('AFTER');
